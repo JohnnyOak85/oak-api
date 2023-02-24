515804687f677ffa7b1b52e73db1d74a6484a61c
JohnnyOak85 <joao.cmp@proton.me>
2023-02-24 21:08:28 +0000

    Fixed log page

--- app/basic/helpers/log.ts
+++ app/basic/helpers/log.ts
@@ -2,7 +2,14 @@ import { ErrorHandler, FileHandler, PathHandler } from '../../tools';
 
 export const getLogFile = () => {
     try {
-        return FileHandler.get(`${PathHandler.logs}/log.txt`).toString('utf8');
+        const page = FileHandler.get(`${PathHandler.resources}/pages/log.html`).toString('utf8');
+        const log = FileHandler.get(`${PathHandler.logs}/log.txt`)
+            .toString('utf8')
+            .split('\n')
+            .map(line => `<p>${line.replace('\r', '')}</p>`)
+            .join('');
+
+        return page.replace('§log', log);
     } catch (error) {
         throw ErrorHandler.wrap(error, 'getLogFile');
     }
