f22849d64cf2d8c7c22694788ae0ffd17da651f4
JohnnyOak85 <joao.cmp@proton.me>
2023-02-24 17:56:31 +0000

    Updates to error handler

--- app/tools/error.ts
+++ app/tools/error.ts
@@ -1,10 +1,19 @@
-import { boomify, notFound } from '@hapi/boom';
+import { boomify, notAcceptable, notFound } from '@hapi/boom';
 import log from './log';
 
 export default {
     notFound: notFound,
     wrap: (error: any, functionName = 'default') => {
         log.error(error, functionName);
+
+        if (error.message.includes('EISDIR')) {
+            throw notAcceptable(error);
+        }
+
+        if (error.message.includes('ENOENT')) {
+            throw notFound(error);
+        }
+
         return boomify(error);
     }
 };
