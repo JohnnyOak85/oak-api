9f44815b261dc60cc64c50301072d01188b1df4b
JohnnyOak85 <joao.cmp@proton.me>
2023-03-12 21:16:44 +0000

    Updates to tools

--- app/plugins/index.ts
+++ app/plugins/index.ts
@@ -1,6 +1,5 @@
 import artemis from '../artemis';
 import basic from '../basic/plugins/basic.plugin';
 import finances from '../finances';
-import news from '../news';
 
-export default [...artemis, basic, ...finances, ...news];
+export default [...artemis, basic, ...finances];
--- app/tools/index.ts
+++ app/tools/index.ts
@@ -1,21 +1,15 @@
 import paths from '../../paths';
 import storage from './storage';
-import certification from './certification';
-import environment from './environment';
 import error from './error';
 import files from './files';
 import identifier from './identifier';
-import log from './log';
 import math from './math';
 import cache from './cache';
 
 export const CacheHandler = cache;
 export const Calculator = math;
-export const Certification = certification;
-export const Environment = environment;
 export const ErrorHandler = error;
 export const FileHandler = files;
 export const Identifier = identifier;
-export const Logger = log;
 export const StorageHandler = storage;
 export const PathHandler = paths;
