8c558f3ed272142c895b8ff9ec87f969e437f6df
JohnnyOak85 <joao.cmp@proton.me>
2023-03-13 18:43:46 +0000

    Removed tools

--- app/tools/files.ts
+++
@@ -1,6 +0,0 @@
-import { readdirSync, readFileSync } from 'fs-extra';
-
-export default {
-    get: (path: string) => readFileSync(path),
-    getList: (path: string) => readdirSync(path)
-};
--- app/tools/math.ts
+++
@@ -1,8 +0,0 @@
-export default {
-    random: (max = 100, min = 1) => Math.floor(Math.random() * (max - min + 1) + min),
-    round: (num: number, places = 2) => {
-        const basePower = Math.pow(10, places);
-
-        return Math.round(num * basePower) / basePower;
-    }
-};
