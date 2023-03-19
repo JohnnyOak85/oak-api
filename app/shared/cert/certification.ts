b7531a084f78b07835a8139e5e7d2e18c363b4ba
JohnnyOak85 <joao.cmp@proton.me>
2023-03-19 19:07:05 +0000

    Removed unused method

--- app/shared/cert/certification.ts
+++ app/shared/cert/certification.ts
@@ -1,15 +1,6 @@
 import { getFile, getVariables } from '..';
 import { certsPath } from '../../../paths';
 
-export const buildCertification = () => {
-    const { certificate } = getVariables();
-
-    return {
-        key: getFile(`${certsPath}/${certificate}-key.pem`),
-        cert: getFile(`${certsPath}/${certificate}-cert.pem`)
-    };
-};
-
 export const getCertifications = () => {
     const { certificate } = getVariables();
 
