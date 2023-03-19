986934160ec8488f2531e2d835a0a7334ef6e677
JohnnyOak85 <joao.cmp@proton.me>
2023-03-19 19:09:31 +0000

    Updated crud tool

--- app/shared/integration/crud.ts
+++ app/shared/integration/crud.ts
@@ -1,12 +1,14 @@
-import Axios from 'axios';
+import Axios, { AxiosRequestConfig } from 'axios';
 import { Dictionary } from '..';
 
-type RequestOptions = {
+interface RequestOptions extends AxiosRequestConfig {
+    data?: any;
     headers?: Dictionary<string>;
-    params?: Dictionary<any>;
+    params?: any;
     url: string;
-};
+}
 
 const client = Axios.create();
 
 export const get = <T>({ url, ...options }: RequestOptions) => client.get<T>(url, options);
+export const put = <T>({ url, data }: RequestOptions) => client.put<T>(url, data);
