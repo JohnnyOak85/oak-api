142d0def9ed8f9ec332f1e62c33e2487ab7462d8
JohnnyOak85 <joao.cmp@proton.me>
2023-03-19 19:08:55 +0000

    Listing valid databases

---
+++ app/shared/enums/index.ts
@@ -0,0 +1,13 @@
+const Databases = {
+    bot: 'artemis',
+    contributors: 'contributors',
+    debts: 'debts',
+    expenses: 'expenses',
+    finances: 'finances',
+    game: 'murwelgrave',
+    news: 'news',
+    redis: 'redis',
+    reminders: 'reminders'
+};
+
+export const ValidDatabases = Object.values(Databases);
