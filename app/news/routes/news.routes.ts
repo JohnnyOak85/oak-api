ea0aec4ed623dbfa149c4a998fc02d9d0260c4d4
JohnnyOak85 <joao.cmp@proton.me>
2023-03-24 21:12:43 +0000

    Notes

--- app/news/routes/news.routes.ts
+++ app/news/routes/news.routes.ts
@@ -3,6 +3,8 @@ import { getDailyGamingNews } from '../handlers';
 
 const prefix = 'news';
 
+// TODO Needs to be shared or a separate integration microservice
+
 export const routes = addPrefix(
     [
         {
